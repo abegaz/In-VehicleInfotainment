d2c07d3300e8c2fda85c1649b5d0a02d8df5f811
Trevor <trevor.h.crow@gmail.com>
2018-10-18 17:04:11 -0400

    Created Controller and added all mutators and accessors for the model
    After this, somethings have to be changed in the model for the temperature
    
    Co-Authored-By: cralle0209 <cralle0209@users.noreply.github.com>

--- AndroidStudio/VehicleInfotainmentSystem/app/src/main/java/Model/HVAC_Model.java
+++ AndroidStudio/VehicleInfotainmentSystem/app/src/main/java/Model/HVAC_Model.java
@@ -1,5 +1,40 @@
 package Model;
 
 public class HVAC_Model {
-    int 
+    private byte temperature = 0; //temperature of vehicle
+    private boolean ac = false, heater = false, recycle_air = false; //all on and off booleans for the rest
+
+    //getters and setters for HVAC variables
+    public String getTemperature() {
+        return temperature+"°";
+    }
+
+    public void setTemperature(byte temperature) {
+        this.temperature = temperature;
+    }
+
+    public boolean isAc() {
+        return ac;
+    }
+
+    public void setAc(boolean ac) {
+        this.ac = ac;
+    }
+
+    public boolean isHeater() {
+        return heater;
+    }
+
+    public void setHeater(boolean heater) {
+        this.heater = heater;
+    }
+
+    public boolean isRecycle_air() {
+        return recycle_air;
+    }
+
+    public void setRecycle_air(boolean recycle_air) {
+        this.recycle_air = recycle_air;
+    }
+
 }
