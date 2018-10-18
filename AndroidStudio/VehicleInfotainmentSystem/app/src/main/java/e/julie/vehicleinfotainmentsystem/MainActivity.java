99f0ccb5506d101e176cba1cabe42f42229b28f2
dandydanderson <davanthander@gmail.com>
2018-10-18 14:03:58 -0400

    Working Menu
    The main menu actually loads now. Fixecd the imageview problem that was causing it to crash.
    
    David

--- AndroidStudio/VehicleInfotainmentSystem/app/src/main/java/e/julie/vehicleinfotainmentsystem/MainActivity.java
+++ AndroidStudio/VehicleInfotainmentSystem/app/src/main/java/e/julie/vehicleinfotainmentsystem/MainActivity.java
@@ -2,15 +2,14 @@ package e.julie.vehicleinfotainmentsystem;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import static e.julie.vehicleinfotainmentsystem.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +20,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -53,5 +44,3 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
-
-//test
