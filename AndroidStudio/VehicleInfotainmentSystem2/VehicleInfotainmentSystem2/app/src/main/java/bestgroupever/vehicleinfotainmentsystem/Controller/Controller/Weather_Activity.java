225b4d4d84abf79ab24a2feda7873570813c48f1
dandydanderson <davanthander@gmail.com>
2018-11-04 16:28:46 -0500

    Weather Back Buton
    functioning back button on the weather

--- AndroidStudio/VehicleInfotainmentSystem2/VehicleInfotainmentSystem2/app/src/main/java/bestgroupever/vehicleinfotainmentsystem/Controller/Controller/Weather_Activity.java
+++ AndroidStudio/VehicleInfotainmentSystem2/VehicleInfotainmentSystem2/app/src/main/java/bestgroupever/vehicleinfotainmentsystem/Controller/Controller/Weather_Activity.java
@@ -1,5 +1,6 @@
 package bestgroupever.vehicleinfotainmentsystem.Controller.Controller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,8 @@ import android.widget.TextView;
 import bestgroupever.vehicleinfotainmentsystem.R;
 
 public class Weather_Activity extends AppCompatActivity {
+
+    Intent intent;
     static ImageView conditionImageView;
     static TextView lowTempTextView;
     static TextView highTempTextView;
@@ -32,6 +35,24 @@ public class Weather_Activity extends AppCompatActivity {
         String city = "Dahlonega";
         DownloadJSON task = new DownloadJSON();
         task.execute("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=d5bb9d4bb6d89bf2b50b7e2c39e5c8cf");
+
+
+    }
+    //this method is used to make the imageViews work. Call this method in the android:onClick in the xml file
+
+    public void changeLayout(View v){
+
+        switch (v.getId()){
+
+
+            case R.id.back_button:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+
+
+
+
+        }
 
 
     }
